3a3dee14d32762cffd889f3040b7b4c75279d8da
JWilson45 <jason.wilson2@marist.edu>
2018-11-07 23:39:39 -0500

    Question 2

---
+++ labs/9/IllegalTriangleException.java
@@ -0,0 +1,10 @@
+public class IllegalTriangleException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4656870708803172249L;
+
+	IllegalTriangleException(String s){
+		super(s);
+	}
+}
